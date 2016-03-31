b76e0433a775cd080c8046edfa1bcebb9c990f18
Ian Gulliver <flamingcow@gmail.com>
2016-03-31 14:59:02 -0700

    Simplify syntax and log

--- client/fetch_and_update.sh
+++ client/fetch_and_update.sh
@@ -7,7 +7,7 @@ mkdir -p "${IMAGES}"
 
 BOOT=/isodevice
 
-FLAGS=$(cat flags)
+FLAGS=$(<flags)
 
 ./fetcher.py --image-dir="${IMAGES}" --ca-cert=../config/ca.cert.pem ${FLAGS}
 ./update_grub.py --image-dir="${IMAGES}" --boot-dir="${BOOT}" > ${BOOT}/grub/grub.cfg.tmp && mv ${BOOT}/grub/grub.cfg.tmp ${BOOT}/grub/grub.cfg
