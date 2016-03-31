ff83b5fb0e13b57beaea7ebd880add498638f3bd
Ian Gulliver <flamingcow@gmail.com>
2016-03-31 11:23:40 -0700

    Split out utility script

---
+++ client/fetch_and_update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -ex
+
+IMAGES=/isodevice/iconograph
+mkdir -p "${IMAGES}"
+
+BOOT=/isodevice
+
+FLAGS=$(cat flags)
+
+./fetcher.py --image-dir="${IMAGES}" --ca-cert=../config/ca.cert.pem ${FLAGS}
+./update_grub.py --image-dir="${IMAGES}" --boot-dir="${BOOT}" > ${BOOT}/grub/grub.cfg.tmp && mv ${BOOT}/grub/grub.cfg.tmp ${BOOT}/grub/grub.cfg
