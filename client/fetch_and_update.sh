51673e491ff76acbcdba796f78f5922e1592101c
Ian Gulliver <flamingcow@gmail.com>
2016-03-31 16:47:34 -0700

    Better handling for pathing

--- client/fetch_and_update.sh
+++ client/fetch_and_update.sh
@@ -2,12 +2,14 @@
 
 set -ex
 
+BASE=$(dirname $0)
+
 IMAGES=/isodevice/iconograph
 mkdir -p "${IMAGES}"
 
 BOOT=/isodevice
 
-FLAGS=$(<flags)
+FLAGS=$(<${BASE}/flags)
 
-./fetcher.py --image-dir="${IMAGES}" --ca-cert=../config/ca.cert.pem ${FLAGS}
-./update_grub.py --image-dir="${IMAGES}" --boot-dir="${BOOT}" > ${BOOT}/grub/grub.cfg.tmp && mv ${BOOT}/grub/grub.cfg.tmp ${BOOT}/grub/grub.cfg
+${BASE}/fetcher.py --image-dir="${IMAGES}" --ca-cert=../config/ca.cert.pem ${FLAGS}
+${BASE}/update_grub.py --image-dir="${IMAGES}" --boot-dir="${BOOT}" > ${BOOT}/grub/grub.cfg.tmp && mv ${BOOT}/grub/grub.cfg.tmp ${BOOT}/grub/grub.cfg
