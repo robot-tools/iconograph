5de9a8d2c62f08c67799826df604de4faae717e3
Ian Gulliver <flamingcow@gmail.com>
2016-04-22 21:11:58 +0000

    Support update_grub flags.

--- client/fetch_and_update.sh
+++ client/fetch_and_update.sh
@@ -9,7 +9,10 @@ mkdir -p "${IMAGES}"
 
 BOOT="/isodevice"
 
-FLAGS="$(cat /icon/config/fetcher.flags)"
+FETCHER_FLAGS="$(cat /icon/config/fetcher.flags)"
+if -f /icon/config/update_grub.flags; then
+  UPDATE_GRUB_FLAGS="$(cat /icon/config/update_grub.flags)"
+fi
 CA_CERT="/icon/config/ca.image.cert.pem"
 
 HTTPS_CLIENT_KEY="/systemid/$(hostname).www.key.pem"
@@ -23,5 +26,5 @@ if test -e "${HTTPS_CA_CERT}"; then
   HTTPS_CA_FLAGS="--https-ca-cert=${HTTPS_CA_CERT}"
 fi
 
-"${BASE}/fetcher.py" --image-dir="${IMAGES}" --ca-cert="${CA_CERT}" ${FLAGS} ${HTTPS_CLIENT_FLAGS} ${HTTPS_CA_FLAGS}
-"${BASE}/update_grub.py" --image-dir="${IMAGES}" --boot-dir="${BOOT}" > "${BOOT}/grub/grub.cfg.tmp" && mv "${BOOT}/grub/grub.cfg.tmp" "${BOOT}/grub/grub.cfg"
+"${BASE}/fetcher.py" --image-dir="${IMAGES}" --ca-cert="${CA_CERT}" ${FETCHER_FLAGS} ${HTTPS_CLIENT_FLAGS} ${HTTPS_CA_FLAGS}
+"${BASE}/update_grub.py" --image-dir="${IMAGES}" --boot-dir="${BOOT}" ${UPDATE_GRUB_FLAGS} > "${BOOT}/grub/grub.cfg.tmp" && mv "${BOOT}/grub/grub.cfg.tmp" "${BOOT}/grub/grub.cfg"
